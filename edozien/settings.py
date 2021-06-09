ba9003899227278a41e28af356c662f03465f92a
Georjay <geonoble988@gmail.com>
2021-06-09 15:50:22 +0100

    Touch ups

--- edozien/settings.py
+++ edozien/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'net',
     'users',
     'ckeditor',
+    'storages',
     'embed_video',
 ]
 
@@ -128,3 +129,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET')
+
+# Had to add these 2 settings for AWS to work. 
+# This should correspond with the region on AWS Bucket
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION')
+AWS_S3_SIGNATURE_VERSION = os.environ.get('AWS_S3_SIGNATURE')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
