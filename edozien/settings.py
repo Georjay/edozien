c77d7cfd2e2d3af6bcd2debb65c1b984f10936aa
Georjay <geonoble988@gmail.com>
2021-06-09 18:09:49 +0100

    Update settings.py

--- edozien/settings.py
+++ edozien/settings.py
@@ -9,12 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jz%(fwob7w1h=2uj&4)yd8kf2v8y3tc_o7_r0p2_au!3h=vp3a'
+# SECRET_KEY = 'django-insecure-jz%(fwob7w1h=2uj&4)yd8kf2v8y3tc_o7_r0p2_au!3h=vp3a'
+SECRET_KEY = os.environ.get('EDOZIEN_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['edozien.herokuapp.com']
 
 
 # Application definition
