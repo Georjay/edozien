b29c6f68fa45275893e03d6aeaac1b60a0b150d4
Georjay <geonoble988@gmail.com>
2021-06-09 18:45:15 +0100

    Update settings.py

--- edozien/settings.py
+++ edozien/settings.py
@@ -14,7 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('EDOZIEN_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
 ALLOWED_HOSTS = ['edozien.herokuapp.com']
 
