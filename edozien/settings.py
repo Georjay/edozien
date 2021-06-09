c699f446df709b28d990b33a277e6f4e7599514c
Georjay <geonoble988@gmail.com>
2021-06-09 18:21:59 +0100

    Updates

--- edozien/settings.py
+++ edozien/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
